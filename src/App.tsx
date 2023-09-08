1d4dc2dc34ee1b25e8eb9ae606e1ffc971728945
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-08 19:36:38 +0800

    style: lint `/src` files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
 		<>
 			
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
