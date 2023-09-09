3386f657f73d84263bf161410c9be2cfd0ad0ee7
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-09 19:52:27 +0800

    feat: set initial `break` & `session` time states

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 
 import "./App.css";
 
-function App() {
+function App(): React.ReactElement {
+	const [break_minutes, set_break_minutes] = useState(5);
+	const [session_minutes, set_session_minutes] = useState(25);
 
 	return (
 		<>
 			<div id="break-time">
 				<div id="break-label">Break Time</div>
+				<div id="break-length">{break_minutes}</div>
 				<button id="break-decrement">v</button>
 				<button id="break-increment">^</button>
 			</div>
 			<div id="session-time">
 				<div id="session-label">Session Time</div>
+				<div id="session-length">{session_minutes}</div>
 				<button id="session-decrement">v</button>
 				<button id="session-increment">^</button>
 			</div>
