0033a06702eeee2075cda8adf1170d5d3a7939c8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-09 19:50:15 +0800

    feat: make `#break-time` and `#session-time`

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ function App() {
 
 	return (
 		<>
-			
+			<div id="break-time">
+				<div id="break-label">Break Time</div>
+				<button id="break-decrement">v</button>
+				<button id="break-increment">^</button>
+			</div>
+			<div id="session-time">
+				<div id="session-label">Session Time</div>
+				<button id="session-decrement">v</button>
+				<button id="session-increment">^</button>
+			</div>
 		</>
 	);
 }
