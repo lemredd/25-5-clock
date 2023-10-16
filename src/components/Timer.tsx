917b4414c299668c3fb4b43602d367212554dc10
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-16 22:49:06 +0800

    refactor(component): assign formatted text `const`
    Variables (`const`) assigned:
    - `formatted_minutes`
    - `formatted_seconds`

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,16 +8,19 @@ export default function Timer({
 	session_minutes,
 	seconds
 }: Props): React.ReactElement {
+	const capitalized_timer_playing = timer_playing[0].toLocaleUpperCase() + timer_playing.substring(1);
+
 	const format_to_two_digits =
 		(number_to_format: number): number | string => number_to_format >= 10
 			? number_to_format
 			: `0${number_to_format}`;
-	const capitalized_timer_playing = timer_playing[0].toLocaleUpperCase() + timer_playing.substring(1);
+	const formatted_minutes = format_to_two_digits(running_minutes ?? session_minutes);
+	const formatted_seconds = format_to_two_digits(seconds);
 
 	return (
 		<div id="timer">
 			<div id="timer-label">{capitalized_timer_playing}</div>
-			<div id="time-left">{format_to_two_digits(running_minutes ?? session_minutes)}:{format_to_two_digits(seconds)}</div>
+			<div id="time-left">{formatted_minutes}:{formatted_seconds}</div>
 		</div>
 	);
 }
